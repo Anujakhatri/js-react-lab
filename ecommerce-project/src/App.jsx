1c59cb508a18a68326b54ea48337cc1525efe78e
Anuja khatri <khatrianuja20@gmail.com>
2026-02-09 14:27:56 +0545

    changes to the correct path

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
-  
+
 
   return (
-      <HomePage />
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
   )
 }
 
