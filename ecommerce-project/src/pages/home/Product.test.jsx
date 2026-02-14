4335f466393454bc93d8bce283c263beabaa942c
Anuja khatri <khatrianuja20@gmail.com>
2026-02-14 16:25:35 +0545

    test: Add unit test for product add to cart functionality.

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,6 +1,10 @@
 import { it, expect, describe, vi } from 'vitest';
 import { Product } from './Product';
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+
+vi.mock('axios'); //fake version of axios so we can't connect to backend
 
 describe('Product component', () => {
     it('renders product name correctly', () => {
@@ -15,7 +19,7 @@ describe('Product component', () => {
             priceCents: 1899,
             keywords: ["kitchen", "kitchen towels", "tissues"]
         };
-        const refreshCart = vi.fn();
+        const refreshCart = vi.fn(); // Mock function to track if it was called
         render(<Product product={product} refreshCart={refreshCart} />);
 
         expect(
@@ -33,5 +37,36 @@ describe('Product component', () => {
         expect(
             screen.getByText('1045')
         ).toBeInTheDocument();
+    });
+
+    it('adds product to cart when add to cart button is clicked', async () => {
+        const product = {
+            id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+            image: "images/products/kitchen-paper-towels-8-pack.jpg",
+            name: "2-Ply Kitchen Paper Towels - 8 Pack",
+            rating: {
+                stars: 4.5,
+                count: 1045
+            },
+            priceCents: 1899,
+            keywords: ["kitchen", "kitchen towels", "tissues"]
+        };
+        const refreshCart = vi.fn();
+
+        // Mock axios.post to resolve successfully
+        axios.post.mockResolvedValue({ data: {} });
+
+        render(<Product product={product} refreshCart={refreshCart} />);
+
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith('/api/cart-items', {
+            productId: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+            quantity: 1,
+        }
+        );
+        expect(refreshCart).toHaveBeenCalled();
     });
 });
